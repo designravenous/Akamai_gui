b3a27ccf5ea45e08c4adacd71c6440f8b058674c
Peter Holgersson <peter.holgersson@hm.com>
2019-03-07 10:37:35 +0100

    error handling for 404 initiated for domain hits

--- application/__init__.py
+++ application/__init__.py
@@ -37,24 +37,26 @@ def index():
                 start = start_day.strftime('%Y%m%d')
                 end_day = form.end_date.data
                 end = end_day.strftime('%Y%m%d')
-                print(credentials[1])
                 result = credentials[0].get(urljoin(credentials[1],'/data-dns/v1/traffic/'+ str(form.domain_name.data) +'?start=' + start + '&start_time=00:00&end=' + end +'&end_time=23:59'))
                 some_text = str(result.text)
-                computer_list = []
-                computer_list.append(some_text)
-                computer_list = [w.replace('\n', ',') for w in computer_list] #List Comprehensions
-                outcome = computer_list[0].split(',')
-                for i in range(5):
-                    outcome.pop(0)
-                count_dns = 0
-                nx_count = 0
-                for hit in outcome[::3]:
-                    count_dns += int(hit)
-                for hit in outcome[1::3]:
-                    nx_count += int(hit)
-                message = "Domain: %s DNS Hits: %d NX Hits:%d Timestamp: %s -> %s" % (form.domain_name.data.upper(),count_dns,nx_count, form.start_date.data,form.end_date.data)
-                hits_info = [form.domain_name.data, count_dns, nx_count, form.start_date.data, form.end_date.data]
-                message = 'Success'
+                if some_text.__contains__("Zone does not exist"):
+                    message = "Zone Does Not Exist"
+                else:
+                    computer_list = []
+                    computer_list.append(some_text)
+                    computer_list = [w.replace('\n', ',') for w in computer_list] #List Comprehensions
+                    outcome = computer_list[0].split(',')
+                    for i in range(5):
+                        outcome.pop(0)
+                    count_dns = 0
+                    nx_count = 0
+                    for hit in outcome[::3]:
+                        count_dns += int(hit)
+                    for hit in outcome[1::3]:
+                        nx_count += int(hit)
+                    message = "Domain: %s DNS Hits: %d NX Hits:%d Timestamp: %s -> %s" % (form.domain_name.data.upper(),count_dns,nx_count, form.start_date.data,form.end_date.data)
+                    hits_info = [form.domain_name.data, count_dns, nx_count, form.start_date.data, form.end_date.data]
+                    message = 'Success'
             except:
                 message = "ERROR occured"
     if form1.validate_on_submit() and form1.submit.data:
